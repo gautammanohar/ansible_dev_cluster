d7d73f6ddcda307e22f49a59c3096a0dc16af70e
Gautam <gautam@shopperstomp.com>
2016-01-20 12:18:08 +0530

    adding selinux rules and encrypted passwords

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "gautam"
+client_key               "#{current_dir}/gautam.pem"
+validation_client_name   "serendio-validator"
+validation_key           "#{current_dir}/serendio-validator.pem"
+chef_server_url          "https://ec2-54-201-70-42.us-west-2.compute.amazonaws.com/organizations/serendio"
+cookbook_path            ["#{current_dir}/../cookbooks"]
---
+++ cookbooks/common/metadata.rb
@@ -0,0 +1,9 @@
+name 'common'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures common'
+long_description 'Installs/Configures common'
+version '0.1.0'
+
+depends 'selinux', '~> 0.9.0'
